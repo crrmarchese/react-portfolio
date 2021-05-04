2d9047f302d4dd1e3224ace936abfe73bff17c19
cmarc <40406039+crrmarchese@users.noreply.github.com>
2021-05-04 18:53:00 -0400

    Added Font Awesome for React package

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,17 +1,24 @@
-import React from 'react'
+import React from "react";
+import { Fade } from "react-awesome-reveal";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLinkedin, faGithubSquare } from "@fortawesome/free-brands-svg-icons";
 
 const Footer = (props:any) => {
    
     return (
         <React.Fragment>
             <footer className="footer mt-auto py-3 bg-light">
-                <div className="container">
+                <div className="container test">
                     <div className="row">
                         <div className="col-12">
-                            <ul className="list-group list-group-horizontal footer-social">
-                                <li className="list-group-item"></li>
-                                <li className="list-group-item"></li>
-                            </ul>
+
+                            <Fade delay={1000} direction="down" cascade> 
+                                <ul className="list-group list-group-horizontal footer-social">
+                                    <li className="list-group-item"><FontAwesomeIcon icon={faLinkedin} size="2x" /></li>
+                                    <li className="list-group-item"><FontAwesomeIcon icon={faGithubSquare} size="2x" /></li>
+                                </ul>
+                            </Fade>
+                            
                         </div>
                     </div>
                 </div>
