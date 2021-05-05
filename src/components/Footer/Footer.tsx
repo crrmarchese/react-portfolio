f51a9526bd873fd82c2a88a7d3a438f7546f135b
cmarc <40406039+crrmarchese@users.noreply.github.com>
2021-05-04 21:18:16 -0400

    Add footer styling

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,20 +2,21 @@ import React from "react";
 import { Fade } from "react-awesome-reveal";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLinkedin, faGithubSquare } from "@fortawesome/free-brands-svg-icons";
+import './styles.css';
 
 const Footer = (props:any) => {
    
     return (
         <React.Fragment>
-            <footer className="footer mt-auto py-3 bg-light">
-                <div className="container test">
+            <footer className="footer mt-auto py-3">
+                <div className="container">
                     <div className="row">
                         <div className="col-12">
 
                             <Fade delay={1000} direction="down" cascade> 
                                 <ul className="list-group list-group-horizontal footer-social">
-                                    <li className="list-group-item"><FontAwesomeIcon icon={faLinkedin} size="2x" /></li>
-                                    <li className="list-group-item"><FontAwesomeIcon icon={faGithubSquare} size="2x" /></li>
+                                    <li className="list-group-item"><a href="https://www.linkedin.com/in/catherinemarchese/" title="LinkedIn" rel="noreferrer"><FontAwesomeIcon icon={faLinkedin} size="4x" /></a></li>
+                                    <li className="list-group-item"><a href="https://github.com/crrmarchese" title="GitHub" rel="noreferrer"><FontAwesomeIcon icon={faGithubSquare} size="4x" /></a></li>
                                 </ul>
                             </Fade>
                             
